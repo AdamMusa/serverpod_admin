706de964ec9ea8ee6cc35e742ab6142f03a8be4b
AdamMusa <adammusa2222@gmail.com>
2026-07-01 07:59:34 -0400

    Update Serverpod dependencies to 3.4.10

--- example/example_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/example_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,25 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import desktop_webview_window
+import file_selector_macos
 import flutter_secure_storage_macos
+import flutter_web_auth_2
 import google_sign_in_ios
 import path_provider_foundation
 import sign_in_with_apple
+import url_launcher_macos
+import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
